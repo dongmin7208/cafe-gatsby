d017b2971780214eb6fe293f81a9873aa0935d0c
dongmin7208 <dongmin.park.career@gmail.com>
2023-02-23 16:20:30 +0900

    ADD:box

--- src/App.js
+++ src/App.js
@@ -1,24 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import styled from 'styled-components';
+
+const Father = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  background-color: #282c34;
+  color: white;
+`;
+const BoxWrapper = styled.div`
+  display: flex;
+`;
+const BoxOne = styled.div`
+  width: 100px;
+  height: 100px;
+  background-color: #282c34;
+  color: white;
+  border: 1px solid white;
+  border-radius: 5px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: 10px;
+  cursor: pointer;
+  &:hover {
+    background-color: #61dafb;
+    color: #282c34;
+  }
+`;
+const BoxTwo = styled.div`
+  width: 100px;
+  height: 100px;
+  background-color: #282c34;
+  color: white;
+  border: 1px solid white;
+  border-radius: 5px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: 10px;
+  cursor: pointer;
+  &:hover {
+    background-color: #61dafb;
+    color: #282c34;
+  }
+`;
+const Text = styled.p`
+  font-size: 20px;
+  font-weight: bold;
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Father>
+      <Text>Styled Components</Text>
+      <BoxWrapper>
+        <BoxOne>Box 1</BoxOne>
+        <BoxTwo>Box 2</BoxTwo>
+      </BoxWrapper>
+    </Father>
   );
 }
 
