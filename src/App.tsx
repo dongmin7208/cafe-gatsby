7f73398d1da30448ba476aa764f0c64359a9af25
dongmin7208 <dongmin.park.career@gmail.com>
2023-03-03 00:56:26 +0900

    ADD:circleVariants

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import styled from 'styled-components';
-import { motion } from 'framer-motion';
+import { motion, useMotionValue, useTransform } from 'framer-motion';
+import { useEffect, useRef } from 'react';
 
 const Wrapper = styled.div`
   height: 100vh;
@@ -7,65 +8,100 @@ const Wrapper = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
+  // flex-direction: column;
+  flex-wrap: wrap;
+`;
+
+const BiggerBox = styled.div`
+  width: 50%;
+  height: 50%;
+  background-color: rgba(255, 255, 255, 0.4);
+  border-radius: 40px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-wrap: wrap;
 `;
 
 const Box = styled(motion.div)`
   width: 200px;
   height: 200px;
-  display: grid;
-  grid-template-columns: repeat(2, 1fr);
-  background-color: rgba(255, 255, 255, 0.2);
+  background-color: rgba(255, 255, 255, 1);
   border-radius: 40px;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
 
-const Circle = styled(motion.div)`
-  background-color: white;
-  height: 70px;
-  width: 70px;
-  place-self: center;
-  border-radius: 35px;
-  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
-`;
-
 const boxVariants = {
-  start: {
-    opacity: 0,
-    scale: 0.5,
-  },
-  end: {
-    scale: 1,
-    opacity: 1,
-    transition: {
-      type: 'spring',
-      duration: 0.5,
-      bounce: 0.5,
-      delayChildren: 0.5,
-      staggerChildren: 0.2,
-    },
-  },
+  hover: { rotateZ: 90 },
+  click: { borderRadius: '100px' },
 };
-
-const circleVariants = {
-  start: {
-    opacity: 0,
-    y: 10,
-  },
-  end: {
-    opacity: 1,
-    y: 0,
-  },
+const framerHover = {
+  hover: { scale: 1.1 },
+  tap: { scale: 0.9 },
 };
 
 function App() {
+  const biggerBoxRef = useRef<HTMLDivElement>(null);
+  const x = useMotionValue(0);
+  const y = useMotionValue(0);
+  const scale = useTransform(x, [-800, 0, 800], [2, 1, 0.1]);
+  const rotateZ = useTransform(y, [-800, 800], [-360, 360]);
+  useEffect(() => {
+    scale.onChange(() => console.log(scale.get()));
+  }, [scale]);
+
   return (
     <Wrapper>
-      <Box variants={boxVariants} initial='start' animate='end'>
-        <Circle variants={circleVariants} />
-        <Circle variants={circleVariants} />
-        <Circle variants={circleVariants} />
-        <Circle variants={circleVariants} />
-      </Box>
+      <button onClick={() => x.set(x.get() + 100)}>right</button>
+      <button onClick={() => x.set(x.get() - 100)}>left</button>
+      <button onClick={() => y.set(y.get() - 100)}>up</button>
+      <button onClick={() => y.set(y.get() + 100)}>down</button>
+      <BiggerBox ref={biggerBoxRef}>
+        <Box
+          style={{ x, y, scale: scale }}
+          drag='x'
+          dragSnapToOrigin
+          dragElastic={0.5}
+          dragConstraints={biggerBoxRef}
+          variants={boxVariants}
+          whileHover='hover'
+          whileTap='click'
+        />
+        <Box
+          style={{ x, y, scale: scale }}
+          drag='x'
+          dragSnapToOrigin
+          dragElastic={0.5}
+          dragConstraints={biggerBoxRef}
+          variants={boxVariants}
+          whileHover='hover'
+          whileTap='click'
+        ></Box>
+        <Box
+          style={{ x, y, scale: scale }}
+          drag='x'
+          dragSnapToOrigin
+          dragElastic={0.5}
+          dragConstraints={biggerBoxRef}
+          variants={boxVariants}
+          whileHover='hover'
+          whileTap='click'
+        ></Box>
+        <Box
+          style={{ x, y, scale: scale }}
+          drag='x'
+          dragSnapToOrigin
+          dragElastic={0.5}
+          dragConstraints={biggerBoxRef}
+          variants={boxVariants}
+          whileHover='hover'
+          whileTap='click'
+        ></Box>
+      </BiggerBox>
+      {/* <BiggerBox ref={biggerBoxRef}>
+        <BiggerBox ref={biggerBoxRef}></BiggerBox>
+        <BiggerBox ref={biggerBoxRef}></BiggerBox>
+      </BiggerBox> */}
     </Wrapper>
   );
 }
